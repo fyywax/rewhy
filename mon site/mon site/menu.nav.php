f251d2e085c4a12187cc1cfcbbadc092678d3463
fyywax <116816675+fyywax@users.noreply.github.com>
2022-10-27 16:22:31 +0200

    Add files via upload

---
+++ mon site/mon site/menu.nav.php
@@ -0,0 +1,16 @@
+	<nav class="menu-nav">
+		<ul>
+			<li class="bouton">
+				<a href="index.html">Acceuil</a>
+			</li>
+			<li class="bouton">
+				<a href="news.html">Actualités</a>
+			</li>
+			<li class="bouton">
+				<a href="ventes.html">Acheter des services</a>
+			</li>
+			<li class="bouton">
+				<a href="contact.html">Aide</a>
+			</li>
+		</ul>		
+	</nav>
