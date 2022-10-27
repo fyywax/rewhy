55a5f08ecf70e8f3bb2fbef8432199cf833ce9cd
fyywax <116816675+fyywax@users.noreply.github.com>
2022-10-27 15:45:39 +0200

    Add files via upload

---
+++ mon site/ventes.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	
+<title>Acheter des services</title>
+
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="test/css/styles.css">
+
+</head>
+
+<body>
+	<nav class="menu-nav">
+		<ul>
+			<li class="bouton">
+				<a href="accueil.php">Acceuil</a>
+			</li>
+			<li class="bouton">
+				<a href="news.php">Actualités</a>
+			</li>
+			<li class="bouton">
+				<a href="ventes.php">Acheter des services</a>
+			</li>
+			<li class="bouton">
+				<a href="contact.php">Aide</a>
+			</li>
+			<li class="bouton">
+				<a href="deco.php">Déconexion</a>				
+			</li>
+		</ul>		
+	</nav>
+
+</body>
+
+</html>
